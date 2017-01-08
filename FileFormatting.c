7873782f1e6d9daa07cbc0fc80839e27e85be63c
CaptainDaVinci <yashkothari199767@gmail.com>
2017-01-08 21:31:45 +0530

    Added error checking

--- FileFormatting.c
+++ FileFormatting.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "whiteSpacing.c"
 #include "lineFolding.c"
 
@@ -11,15 +12,28 @@ int main(void)
     p = fopen("original.txt", "r");      //original file containing input
     q = fopen("formatted.txt", "w");     // file which will contain formatted output
 
+    if(p == NULL || q == NULL)
+    {
+        printf("ERROR! Unable to open file\n"); // exit out of the program if file does not exist.
+        exit(1);
+    }
+
     while((c = getc(p)) != EOF)
     {
         folding(c);
     }
+
     fclose(p);
     fclose(q);
 
     p = fopen("formatted.txt", "r");  // this results in output given to the original file itself
     q = fopen("original.txt", "w");
+
+    if(p == NULL || q == NULL)
+    {
+        printf("ERROR! Unable to open file\n");
+        exit(1);
+    }
 
     while((c = getc(p)) != EOF)
     {
