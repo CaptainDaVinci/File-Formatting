ef38760b4d38acac5ab2c9edccdff4847a36237e
CaptainDaVinci <yashkothari199767@gmail.com>
2017-01-19 22:31:46 +0530

    Fixed indentation

--- File - Formatting/FileFormatting.c
+++ File - Formatting/FileFormatting.c
@@ -7,13 +7,13 @@ char c;
 
 int main(int argc, char *argv[])
 {
-    if(argc != 3)
+    if(argc !=  3)
     {
         printf("Usage : %s Input-filename Output-filename\n", argv[0]);
         exit(-1);
     }
 
-    FILE *infile = fopen(argv[1], "r");                //original file containing input
+    FILE *infile = fopen(argv[1], "r");               //original file containing input
     FILE *outfile = fopen(argv[2], "w");
 
     if(infile == NULL || outfile == NULL)          //error checking, also asks user if file is to be created.
